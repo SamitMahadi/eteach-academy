78a50b130c3adbc752db6089da3fe29871269270
SamitMahadi <samithossainmahadi@gmail.com>
2022-10-26 12:07:07 -0400

    Carusel solved

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,47 +1,44 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import photo1 from '../../../Assets/Carusel/pexels-christina-morillo-1181263.jpg'
-import photo2 from '../../../Assets/Carusel/pexels-christina-morillo-1181298.jpg'
-import photo3 from '../../../Assets/Carusel/pexels-christina-morillo-1181467.jpg'
+import photo2 from '../../../Assets/Carusel/new2.jpg'
+import photo3 from '../../../Assets/Carusel/new3.jpg'
 
 const Home = () => {
     return (
         <Carousel>
         <Carousel.Item>
           <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=First slide&bg=373940"
+            className="d-block w-100 "
+            src={photo1}
             alt="First slide"
           />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <Carousel.Caption className='d-flex flex-column justify-content-center h-100 h1'>
+            <h3 className='h1 font-weight-bold'>Welcome To eTeach Academy</h3>
+            
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Second slide&bg=282c34"
+            className="d-block w-100 h-50"
+            src={photo2}
             alt="Second slide"
           />
   
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <Carousel.Caption className='d-flex flex-column justify-content-center h-100 h1'>
+            <h3 className='h1 font-weight-bold'>Best E-learning Platform</h3>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Third slide&bg=20232a"
+            className="d-block w-100 h-50"
+            src={photo3}
             alt="Third slide"
           />
   
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
+          <Carousel.Caption className='d-flex flex-column justify-content-center h-100 h1'>
+            <h3 className='h1 font-weight-bold'>Learn best from the best</h3>
+            
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
